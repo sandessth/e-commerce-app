39ab8684f9df840b25f94f1768ea1fe6d83f5562
Sandesh Shrestha <sandes.sth@gmail.com>
2023-08-20 00:31:30 -0700

    fetch using rtk query

--- frontend/src/app/components/layout/product-list1.tsx
+++ frontend/src/app/components/layout/product-list1.tsx
@@ -1,13 +1,22 @@
 
 "use client"
-import { useAppSelector } from "@/redux/store/hooks";
+import { useGetAllProductsQuery } from "@/redux/features(slices)/products/productsApi";
 
 function ProductList1 (){
-    const product = useAppSelector((state) => {
-        console.log(state.products)
-    })
-    return(
-        <div>new list</div>
+    const {data, error, isLoading} = useGetAllProductsQuery()
+    console.log(data)
+    
+    return isLoading ? (
+        <div>Loading...</div>
+    ) : error ? (
+        <div className="flex justify-center text-2xl m-2 mt-4">error loading</div>
+    ) : ( 
+        <div className="grid grid-cols-4">
+            <div className="col-span-1">
+            {data.map((products) =>((products.name)))}
+        
+        </div>
+        </div>
     )
     
 }
