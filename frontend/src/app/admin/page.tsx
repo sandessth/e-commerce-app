3058145a6f22492d35a1f5aeed99028b6098252e
Sandesh Shrestha <sandes.sth@gmail.com>
2023-09-04 22:42:49 -0700

    change in product description

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -15,7 +15,7 @@ function Admin() {
   const [rating, setRating] = useState<number>(0);
   // const [in_stock, setInStock] = useState<boolean>("");
   const [color, setColor] = useState<string>("");
-  const [model, setModel] = useState<string>("");
+  const [Model, setModel] = useState<string>("");
 
   const router = useRouter();
 
@@ -40,7 +40,7 @@ function Admin() {
       !stock ||
       !image ||
       !color ||
-      !model
+      !Model
     ) {
       toast.error("Please input all fields");
       return;
@@ -56,7 +56,7 @@ function Admin() {
         stock,
         image,
         color,
-        model,
+        Model,
       })
       .then((response) => {
         toast.success("Product added successfully");
@@ -218,11 +218,11 @@ function Admin() {
                       </label>
                       <div className="mt-2 mb-5">
                         <input
-                          value={model}
+                          value={Model}
                           onChange={(event) => setModel(event.target.value)}
                           type="text"
-                          name="model"
-                          id="model"
+                          name="Model"
+                          id="Model"
                           className="block dark:bg-slate-200/20 dark:text-slate-100 px-1 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
                         />
                       </div>
